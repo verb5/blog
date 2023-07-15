325611d3f19d89a0c0e50e3c4a50eec0f381eb16
Daniel Dimitrov <daniel.dimitrov@amusnet.com>
2023-07-15 07:57:04 +0300

    output system data

--- main.go
+++ main.go
@@ -6,14 +6,13 @@ import (
 	"fmt"
 	useful "github.com/verb5/blog/utils"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/utils"
 )
 
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		body := fmt.Sprintf(useful.)
-		return c.SendString()
+		body := fmt.Sprintf("%v\n%v\n",useful.GetHostname(),useful.GetIPAddress())
+		return c.SendString(body)
 
 	})
 	app.Listen(":3000")
